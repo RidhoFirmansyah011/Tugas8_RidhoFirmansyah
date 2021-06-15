c7dc02039c1ac47b8d47111e96db1e1fd61b0f81
RidhoFirmansyah011 <82030417+RidhoFirmansyah011@users.noreply.github.com>
2021-06-15 17:40:45 +0700

    Add files via upload

---
+++ Pemeriksaan_Ridho.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2021 at 12:22 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pemeriksaan_ridho`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `No`
+--
+-- Error reading structure for table pemeriksaan_ridho.No: #1146 - Table 'pemeriksaan_ridho.no' doesn't exist
+-- Error reading data for table pemeriksaan_ridho.No: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `pemeriksaan_ridho`.`No`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
